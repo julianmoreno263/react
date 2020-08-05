04820210bf9096700f621a9c58e687cce5bf1ba9
julian Moreno <julianmoreno263@gmail.com>
2020-08-05 15:17:37 -0500

    Babel Eslint gitignore y otros

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../assets/styles/App.scss";//importamos el archivo de estilo css
+
+const HelloWorld=()=>(
+    <h1>Hola Mundo en React</h1>
+)
+    
+
+
+
+export default HelloWorld
